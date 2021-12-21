e5b1408ce56382d5ef7ad27f1feee0795b17a3eb
Jacob Messore <jacobmessore0@gmail.com>
2021-12-21 00:35:24 -0500

    Added a newsletter functionality with email sign up

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
 
 
 
-function myFunction() {
-  var txt;
-  if (confirm("Press a button!")) {
-    txt = "You pressed OK!";
-  } else {
-    txt = "You pressed Cancel!";
-  }
-  document.getElementById("demo").innerHTML = txt;
-}
+
     
